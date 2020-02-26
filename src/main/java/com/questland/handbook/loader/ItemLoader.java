c3aa25ed95f386995d72679e223bf80f9fad7b6a
mcf <mcf@spotify.com>
2020-02-26 09:41:30 -0500

    add new gear to whitelist

--- src/main/java/com/questland/handbook/loader/ItemLoader.java
+++ src/main/java/com/questland/handbook/loader/ItemLoader.java
@@ -43,6 +43,8 @@ public class ItemLoader implements ApplicationRunner {
       "http://gs-bhs-wrk-01.api-ql.com/staticdata/key/en/android/%s/static_passive_skills/";
 
   private Set<String> manuallyWhitelistedNames = Set.of(
+      "Oni Warrior Armor",
+      "Oni Grabbers",
       "Triumphant Crown",
       "Total Triumph Carapace",
       "Gloves of Total Triumph",
