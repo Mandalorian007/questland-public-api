f8820d52da1c441ca5329933c3d1d18e84e9044f
mcf <mcf@spotify.com>
2020-03-18 11:51:44 -0400

    add oni weapon and talisman

--- src/main/java/com/questland/handbook/loader/ItemLoader.java
+++ src/main/java/com/questland/handbook/loader/ItemLoader.java
@@ -43,6 +43,8 @@ public class ItemLoader implements ApplicationRunner {
       "http://gs-bhs-wrk-01.api-ql.com/staticdata/key/en/android/%s/static_passive_skills/";
 
   private Set<String> manuallyWhitelistedNames = Set.of(
+      "Oni Totem",
+      "Demon's Dame",
       "Necklace of Serendipity",
       "Ring of Folklore",
       "Oni Horns",
