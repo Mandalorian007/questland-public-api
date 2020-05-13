fdd10509549676ab983a4c6cadb88be93c1208e1
mcf <mcf@spotify.com>
2020-05-13 08:29:06 -0400

    update whitelisting

--- src/main/java/com/questland/handbook/loader/ItemLoader.java
+++ src/main/java/com/questland/handbook/loader/ItemLoader.java
@@ -43,6 +43,8 @@ public class ItemLoader implements ApplicationRunner {
       "http://gs-bhs-wrk-01.api-ql.com/staticdata/key/en/android/%s/static_passive_skills/";
 
   private Set<String> manuallyWhitelistedNames = Set.of(
+      "Warfists of Thunder",
+      "Heart of the Storm",
       "Healing Spark",
       "Knight of the Tempest Armor",
       "Helm of the Storm",
