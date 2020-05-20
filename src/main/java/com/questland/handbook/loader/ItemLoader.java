8f1a5bb86c0631ac292e32a44dd7e0d8ae7a4655
mcf <mcf@spotify.com>
2020-05-20 09:43:39 -0400

    add new api items

--- src/main/java/com/questland/handbook/loader/ItemLoader.java
+++ src/main/java/com/questland/handbook/loader/ItemLoader.java
@@ -43,6 +43,8 @@ public class ItemLoader implements ApplicationRunner {
       "http://gs-bhs-wrk-01.api-ql.com/staticdata/key/en/android/%s/static_passive_skills/";
 
   private Set<String> manuallyWhitelistedNames = Set.of(
+      "Booming Blade",
+      "Sphere of the Tempest",
       "Warfists of Thunder",
       "Heart of the Storm",
       "Healing Spark",
