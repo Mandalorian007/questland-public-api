67a24eb21cfeb6a94221757c5e4819974b3ec4d5
mcf <mcf@spotify.com>
2020-05-25 13:19:52 -0400

    adding thunderclap

--- src/main/java/com/questland/handbook/loader/ItemLoader.java
+++ src/main/java/com/questland/handbook/loader/ItemLoader.java
@@ -43,6 +43,7 @@ public class ItemLoader implements ApplicationRunner {
       "http://gs-bhs-wrk-01.api-ql.com/staticdata/key/en/android/%s/static_passive_skills/";
 
   private Set<String> manuallyWhitelistedNames = Set.of(
+      "Thunderclap",
       "Booming Blade",
       "Sphere of the Tempest",
       "Warfists of Thunder",
