846dc50068b5fa945716340c6c8c6376932bb80a
mcf <mcf@spotify.com>
2020-06-19 11:43:12 -0400

    unleash the hero API

--- src/main/java/com/questland/handbook/controller/HeroEndpoints.java
+++ src/main/java/com/questland/handbook/controller/HeroEndpoints.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
-//TODO remove when we want the feature live for everyone
-@ApiIgnore
 @RestController
 @RequiredArgsConstructor
 public class HeroEndpoints {
