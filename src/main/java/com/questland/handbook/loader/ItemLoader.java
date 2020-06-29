9a3882ac657edbb4b20ba89e1701fef69079e68d
mcf <mcf@spotify.com>
2020-06-29 18:35:38 -0400

    whitelist more items

--- src/main/java/com/questland/handbook/loader/ItemLoader.java
+++ src/main/java/com/questland/handbook/loader/ItemLoader.java
@@ -43,6 +43,9 @@ public class ItemLoader implements ApplicationRunner {
       "http://gs-bhs-wrk-01.api-ql.com/staticdata/key/en/android/%s/static_passive_skills/";
 
   private Set<String> manuallyWhitelistedNames = Set.of(
+      "Dread Captain’s Mantle",
+      "Cursed Plank Walkers",
+      "Seafarer's Compass",
       "Ring of the Cursed Depths",
       "The Lost Helm",
       "Grips of the Depths",
