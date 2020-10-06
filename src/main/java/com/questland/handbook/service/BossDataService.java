a468140a506601c86016cc9ff21f4864ba300bcf
Mandalorian007 <mandalorian007@gmail.com>
2020-10-06 19:06:48 -0400

    clean up boss stat loading permanently

--- src/main/java/com/questland/handbook/service/BossDataService.java
+++ src/main/java/com/questland/handbook/service/BossDataService.java
@@ -5,13 +5,14 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.questland.handbook.publicmodel.BossStats;
 import com.questland.handbook.service.model.bossdata.BossStatsCsv;
-import java.io.File;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
 import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Service;
 
 @Service
 public class BossDataService {
@@ -23,17 +24,17 @@ public class BossDataService {
     if (guildBossStatsCache != null) {
       return guildBossStatsCache;
     }
-    Map<BigInteger, BossStats> guildBossStats = loadGuildBossStats().stream()
+    Map<BigInteger, BossStats> guildBossStats = loadBossStats("guild-boss-stats.csv").stream()
         .collect(Collectors.toMap(BossStatsCsv::getLevel, this::convert));
     guildBossStatsCache = guildBossStats;
     return guildBossStats;
   }
 
   public Map<BigInteger, BossStats> getHardBossStats() {
-    if (guildBossStatsCache != null) {
-      return guildBossStatsCache;
+    if (hardBossStatsCache != null) {
+      return hardBossStatsCache;
     }
-    Map<BigInteger, BossStats> hardBossStats = loadHardBossStats().stream()
+    Map<BigInteger, BossStats> hardBossStats = loadBossStats("hard-boss-stats.csv").stream()
         .collect(Collectors.toMap(BossStatsCsv::getLevel, this::convert));
     hardBossStatsCache = hardBossStats;
     return hardBossStats;
@@ -49,26 +50,13 @@ public class BossDataService {
     return stats;
   }
 
-  private List<BossStatsCsv> loadGuildBossStats() {
+  private List<BossStatsCsv> loadBossStats(String resourceFileName) {
     try {
       CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader();
       CsvMapper mapper = new CsvMapper();
-      File file = new ClassPathResource("guild-boss-stats.csv").getFile();
+      InputStream hbStats = new ClassPathResource(resourceFileName).getInputStream();
       MappingIterator<BossStatsCsv> readValues =
-          mapper.readerFor(BossStatsCsv.class).with(bootstrapSchema).readValues(file);
-      return readValues.readAll();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private List<BossStatsCsv> loadHardBossStats() {
-    try {
-      CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader();
-      CsvMapper mapper = new CsvMapper();
-      File file = new ClassPathResource("hard-boss-stats.csv").getFile();
-      MappingIterator<BossStatsCsv> readValues =
-          mapper.readerFor(BossStatsCsv.class).with(bootstrapSchema).readValues(file);
+              mapper.readerFor(BossStatsCsv.class).with(bootstrapSchema).readValues(hbStats);
       return readValues.readAll();
     } catch (Exception e) {
       throw new RuntimeException(e);
